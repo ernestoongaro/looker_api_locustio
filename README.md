3cb8fdf3c29421d9a0b0918b7631144f1fe8584c
Drew Gillson <drew@liveoutthere.com>
2018-09-24 22:32:56 -0600

    Implemented LocustIO load testing of Looker API

---
+++ README.md
@@ -0,0 +1,20 @@
+# Load Testing Looker's API with LocustIO
+This locustfile can be used to generate many simultaneous requests that download look results from the Looker API.
+
+## How to get started
+- [Install LocustIO](https://docs.locust.io/en/stable/installation.html)
+- Create a config.yml file with your API connection credentials
+- From your working directory, start `locust --host=https://universityofcalgary.looker.com` _(the host must match the name of a node in your config.yml file)_
+    - You can also run locust [distributed](https://docs.locust.io/en/stable/running-locust-distributed.html) using multiple workers to really shake things out
+- Now, visit [http://127.0.0.1:8089](http://127.0.0.1:8089) to summon some locusts
+    - You can simulate hundreds of simultaneous users
+    - Each user will request results for random looks from your instance 
+    - If you want to change the behaviour, modify [locustfile.py](locustfile.py)
+    
+### Request Statistics
+The LocustIO web interface will show you request timing statistics for each look:
+![Screenshot of requests](images/requests.png)
+
+### Performance Charts
+As well as correlated charts for requests per second, response time, and number of users:
+![Screenshot of charts](images/charts.png)
---
+++ locustfile.py
@@ -0,0 +1,53 @@
+import yaml
+from locust import HttpLocust, TaskSet, task
+import random
+import requests
+
+class ApiBehavior(TaskSet):
+
+    def on_start(self):
+        global options
+        f = open('config.yml')
+        params = yaml.load(f)
+        f.close()
+        host_yaml_node = self.locust.host
+        self.hostname = params['hosts'][host_yaml_node]['host']
+        self.token = params['hosts'][host_yaml_node]['token']
+        self.secret = params['hosts'][host_yaml_node]['secret']
+        self.login()
+
+    def on_stop(self):
+        self.logout()
+
+    def login(self):
+        url = '{}{}'.format(self.hostname, 'login')
+        params = {'client_id': self.token,
+                  'client_secret': self.secret}
+        with self.client.post(url, params=params, catch_response=True) as r:
+            access_token = r.json().get('access_token')
+            print('Access token: ' + access_token)
+
+        self.client.headers.update({'Authorization': 'token {}'.format(access_token)})
+
+    def logout(self):
+        return
+
+    @task(1)
+    def get_look(self):
+
+        # Choose a random look to retrieve results for:
+        max_look_id = 9  # This is the maximum look ID on my instance - update it as appropriate
+        look_to_get = random.randint(1, max_look_id)
+        url = '{}{}/{}/run/{}'.format(self.hostname, 'looks', look_to_get, 'json')
+
+        params = {'limit': 100000}
+        with self.client.get(url, params=params, stream=True) as r:
+            if r.status_code == requests.codes.ok:
+                print(url + ': success (200)')
+            else:
+                print(url + ': failure (' + r.status_code + ')')
+
+class ApiUser(HttpLocust):
+    task_set = ApiBehavior
+    min_wait = 5000
+    max_wait = 9000
