a4e7fcca0c2037f698b992e0d56598b755713a4c
Ernesto Ongaro <ongaro@google.com>
2020-11-02 13:14:31 +0000

    use Looker SDK

--- locustfile.py
+++ locustfile.py
@@ -1,49 +1,33 @@
-import yaml
-from locust import HttpUser, TaskSet, task, User
-import random
-import requests
+import time,random,requests
+import looker_sdk
+from looker_sdk import models, error
+from locust import User, task, between, events
 
-class ApiBehavior(TaskSet):
+class lookerStress(User):
+    wait_time = between(30, 60)
 
     def on_start(self):
-        self.hostname = 'https://YOURS.cloud.looker.com/api/3.1/'
-        self.secret = 'YOURS'
-        self.token = 'YOURS'
-        self.login()
+        self.sdk = looker_sdk.init31("looker.ini")
 
     def on_stop(self):
-        self.logout()
-
-    def login(self):
-        url = '{}{}'.format(self.hostname, 'login')
-        params = {'client_id': self.token,
-                  'client_secret': self.secret}
-        with self.client.post(url,
-                              params=params,
-                              catch_response=True) as r: # add verify=False if using a self-signed cert
-            access_token = r.json().get('access_token')
-        self.client.headers.update({'Authorization': 'token {}'.format(access_token)})
-
-    def logout(self):
-        self.client.delete(self.hostname+'logout')
-        return
+        self.sdk.logout()
 
     @task(1)
     def get_look(self):
+        api_call_name = 'run_look'
         # Choose a random look to retrieve results for:
-        look_ids = [5,6,7,8,9]  
+        look_ids = [5,6,7,8,9]
         look_to_get = random.choice(look_ids)
-        url = '{}{}/{}/run/{}'.format(self.hostname, 'looks', look_to_get, 'txt')
+        start_time = time.time()
+        try:
+            self.sdk.run_look(look_to_get,'txt')
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(
+                    request_type="GET", name=api_call_name, response_time=total_time, response_length=0
+                    )
+        except error.SDKError as e:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(
+                    request_type="GET", name=api_call_name, response_time=total_time, response_length=0, exception=e
+                )
 
-        params = {'cache': 'false'}
-        #with self.client.get(url, params=params, stream=True) as r:
-        with self.client.get(url, params=params,timeout=60) as r:
-            if r.status_code == requests.codes.ok:
-                print(url + ': success (200)')
-            else:
-                print(url + ': failure (' + str(r.status_code) + str(r.reason) + ')')
-
-class ApiUser(HttpUser):
-    tasks = [ApiBehavior]
-    min_wait = 10000
-    max_wait = 60000
